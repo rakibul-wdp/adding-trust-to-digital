44c1b10748961d9d29faf9b88fa27a35d2b9fd92
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-09 05:43:50 +0600

    create layout with help of react router outlet

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../pages/Home/Footer/Footer';
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
